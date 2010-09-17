acf6229d135c4821b3e66b5e185356419f0d5a03
dojo-se <dojo-se@googlegroups.com>
2010-09-17 20:27:16 -0300

    FizzBuzz funcionando de 0 ate 10. (Tiago)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,9 +4,9 @@ def fizzbuzz(number):
     fizz = []
     for i in range(number):
         if i != 0 and i % 3 == 0:
-            fizz.append('fizz')
+            fizz.append('Fizz')
         elif i != 0 and i % 5 == 0:
-            fizz.append('buzz')
+            fizz.append('Buzz')
         else:
             fizz.append(i)
     
@@ -17,10 +17,16 @@ class StubTests(unittest.TestCase):
         self.assertEquals(fizzbuzz(3), [0,1,2])
     
     def testFizz0_3(self):
-        self.assertEquals(fizzbuzz(4), [0,1,2,'fizz'])
+        self.assertEquals(fizzbuzz(4), [0,1,2,'Fizz'])
     
     def testFizz0_5(self):
-        self.assertEquals(fizzbuzz(6), [0,1,2,'fizz',4,'buzz'])
+        self.assertEquals(fizzbuzz(6), [0,1,2,'Fizz',4,'Buzz'])
+    
+    def testFizz0_6(self):
+        self.assertEquals(fizzbuzz(7), [0,1,2,'Fizz',4,'Buzz','Fizz'])
+        
+    def testFizz0_10(self):
+        self.assertEquals(fizzbuzz(11), [0,1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz'])
 
 if __name__ == '__main__':
     unittest.main()
