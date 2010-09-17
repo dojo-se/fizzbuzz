b277a3f2b2e6fb823a1c11f7590e0bbe788f2c5a
dojo-se <dojo-se@googlegroups.com>
2010-09-17 20:23:16 -0300

    FizzBuzz funcionando de 0 ate 5. (Tiago)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,15 +5,22 @@ def fizzbuzz(number):
     for i in range(number):
         if i != 0 and i % 3 == 0:
             fizz.append('fizz')
-            continue
-        fizz.append(i)
+        elif i != 0 and i % 5 == 0:
+            fizz.append('buzz')
+        else:
+            fizz.append(i)
+    
     return fizz
 
 class StubTests(unittest.TestCase):
     def testFizz0_2(self):
         self.assertEquals(fizzbuzz(3), [0,1,2])
+    
     def testFizz0_3(self):
         self.assertEquals(fizzbuzz(4), [0,1,2,'fizz'])
+    
+    def testFizz0_5(self):
+        self.assertEquals(fizzbuzz(6), [0,1,2,'fizz',4,'buzz'])
 
 if __name__ == '__main__':
     unittest.main()
