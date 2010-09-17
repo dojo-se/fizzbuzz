6a75e306614bd710f98ccff45ed74a178a6e04c2
dojo-se <dojo-se@googlegroups.com>
2010-09-17 20:35:23 -0300

    Ajuste de inputs para adequar aos prerequisitos

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,10 +2,10 @@ import unittest
 
 def fizzbuzz(number):
     fizz = []
-    for i in range(number):
-        if i != 0 and i % 3 == 0:
+    for i in range(1, number+1):
+        if i % 3 == 0:
             fizz.append('Fizz')
-        elif i != 0 and i % 5 == 0:
+        elif i % 5 == 0:
             fizz.append('Buzz')
         else:
             fizz.append(i)
@@ -13,20 +13,22 @@ def fizzbuzz(number):
     return fizz
 
 class StubTests(unittest.TestCase):
-    def testFizz0_2(self):
-        self.assertEquals(fizzbuzz(3), [0,1,2])
+    def testFizz1_2(self):
+        self.assertEquals(fizzbuzz(2), [1,2])
     
-    def testFizz0_3(self):
-        self.assertEquals(fizzbuzz(4), [0,1,2,'Fizz'])
+    def testFizz1_3(self):
+        self.assertEquals(fizzbuzz(3), [1,2,'Fizz'])
     
-    def testFizz0_5(self):
-        self.assertEquals(fizzbuzz(6), [0,1,2,'Fizz',4,'Buzz'])
+    def testFizz1_5(self):
+        self.assertEquals(fizzbuzz(5), [1,2,'Fizz',4,'Buzz'])
     
-    def testFizz0_6(self):
-        self.assertEquals(fizzbuzz(7), [0,1,2,'Fizz',4,'Buzz','Fizz'])
+    def testFizz1_6(self):
+        self.assertEquals(fizzbuzz(6), [1,2,'Fizz',4,'Buzz','Fizz'])
         
-    def testFizz0_10(self):
-        self.assertEquals(fizzbuzz(11), [0,1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz'])
+    def testFizz1_10(self):
+        self.assertEquals(fizzbuzz(10), [1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz'])
+    
 
 if __name__ == '__main__':
     unittest.main()
+
